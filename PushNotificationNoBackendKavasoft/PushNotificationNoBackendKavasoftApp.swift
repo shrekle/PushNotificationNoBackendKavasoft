ae786ab44454e7dc85eac545685421e5a832ce1e
shrekle <nanonline760@gmail.com>
2023-05-11 09:37:32 -0800

    Initial Commit

---
+++ PushNotificationNoBackendKavasoft/PushNotificationNoBackendKavasoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationNoBackendKavasoftApp.swift
+//  PushNotificationNoBackendKavasoft
+//
+//  Created by nan on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationNoBackendKavasoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
